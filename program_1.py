a75b321b032a1a5727ad6580e52eef5da140ccd9
henryf12345687 <hjforst@students.unwsp.edu>
2025-09-12 11:15:36 -0500

    Update program_1.py
    Henry Forst
    9/12/25

--- program_1.py
+++ program_1.py
@@ -1,5 +1,16 @@
+# Henry Forst 
+#09/10/25
+# Assignment 2
+name = "Henry Forst"
+address = "6709, 51st st Lebron, Georgia, 59087"
+imagenary_number = "560-789-0000"
+college_major = "sports analytics"
+#multiple print functions to make look like a list 
 def personal_information():
-    print('...')
+    print(name)
+    print(address)
+    print(imagenary_number)
+    print(college_major)
 
 
 # Line which calls the above function.
