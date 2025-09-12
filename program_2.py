79c993d8fec863bd47c4dc4995ca0f30a6f9872a
henryf12345687 <hjforst@students.unwsp.edu>
2025-09-12 11:17:01 -0500

    Update program_2.py
    Henry Forst
    9/12/25

--- program_2.py
+++ program_2.py
@@ -1,13 +1,20 @@
-
+# Henry Forst 
+#09/10/25
+# Assignment 2
 def average_age():
     # Get User Input
-
+    age1 = int(input("Enter the age of friend 1:"))
+    age2 = int(input("Enter the age of friend 2:"))
+    age3 = int(input("Enter the age of friend 3:"))
+    age4 = int(input("Enter the age of friend 4:"))
+    age5 = int(input("Enter the age of friend 5:"))
 
     # Sum ages
-
+    sum_age = age1 + age2 + age3 + age4 + age5
     # Average the ages
-
+    average_age = sum_age / 5
     # Print the results
-
+    print("The sum age is:", sum_age)
+    print("The average age is:", average_age)
 # Line which calls the above function.
 average_age()
